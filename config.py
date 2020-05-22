8ffb4a3f024fdcaf267d08f26292040c1de35091
Martin <martinwjwilson@gmail.com>
2020-05-22 14:32:02 +0100

    coloured emojis

--- config.py
+++ config.py
@@ -10,6 +10,17 @@ channel_dict = {
     "waifu_roulette"                    : 620872909803487234
 }
 
+
+blue_emoji_id = "<:Blue:621119170049736723>"
+black_emoji_id = "<:Black:621119169785495563>"
+yellow_emoji_id = "<:Yellow:621119170590933002>"
+pink_emoji_id = "<:Pink:621119170297462803>"
+red_emoji_id = "<:Red:621119170528018433>"
+white_emoji_id = "<:White:621119170729476115>"
+orange_emoji_id = "<:Orange:621119170637201408>"
+green_emoji_id = "<:Green:621119169617985558>"
+purple_emoji_id = "<:Purple:621119170985197588>"
+
 # ROLES
 role_dict = {
     # users
